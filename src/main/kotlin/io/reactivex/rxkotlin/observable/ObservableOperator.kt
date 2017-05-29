8ea8ff6a004f559dd960409f6911c05b2d7ed049
Thomas Nield <thomasnield@live.com>
2017-05-28 21:33:12 -0500

    implement filter() and map()

--- src/main/kotlin/io/reactivex/rxkotlin/observable/ObservableOperator.kt
+++ src/main/kotlin/io/reactivex/rxkotlin/observable/ObservableOperator.kt
@@ -1,2 +1,38 @@
 package io.reactivex.rxkotlin.observable
 
+import org.reactivestreams.Subscriber
+import org.reactivestreams.Subscription
+
+inline fun <T,R> Observable<T>.map(crossinline mapper: (T) -> R): Observable<R> = object: Observable<R> {
+
+    private val upstream: Observable<T> = this@map
+
+    override fun subscribe(subscriber: Subscriber<in R>) {
+        upstream.subscribe(object: Subscriber<T> {
+            override fun onComplete() = subscriber.onComplete()
+
+            override fun onNext(t: T) = subscriber.onNext(mapper(t))
+
+            override fun onError(t: Throwable) = subscriber.onError(t)
+
+            override fun onSubscribe(s: Subscription) = subscriber.onSubscribe(s)
+        })
+    }
+}
+
+inline fun <T> Observable<T>.filter(crossinline predicate: (T) -> Boolean): Observable<T> = object: Observable<T> {
+
+    private val upstream: Observable<T> = this@filter
+
+    override fun subscribe(subscriber: Subscriber<in T>) {
+        upstream.subscribe(object: Subscriber<T> {
+            override fun onComplete() = subscriber.onComplete()
+
+            override fun onNext(t: T) = if (predicate(t)) subscriber.onNext(t) else Unit
+
+            override fun onError(t: Throwable) = subscriber.onError(t)
+
+            override fun onSubscribe(s: Subscription) = subscriber.onSubscribe(s)
+        })
+    }
+}
--- src/test/kotlin/Playground.kt
+++ src/test/kotlin/Playground.kt
@@ -1,8 +1,10 @@
 import io.reactivex.rxkotlin.observable.Observable
-import io.reactivex.rxkotlin.observable.just
+import io.reactivex.rxkotlin.observable.*
 import io.reactivex.rxkotlin.subscribe
 
 fun main(args: Array<String>) {
     Observable.just("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
+            .map { it.length }
+            .filter { it >= 5}
             .subscribe { println(it) }
 }
