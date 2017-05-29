9610c9af8f3e00c671cc8ecbb274f0748577d911
Thomas Nield <thomasnield@live.com>
2017-05-28 22:38:14 -0500

    make Observable.defer() inline

--- src/main/kotlin/io/reactivex/rxkotlin/observable/ObservableFactory.kt
+++ src/main/kotlin/io/reactivex/rxkotlin/observable/ObservableFactory.kt
@@ -24,7 +24,7 @@ fun <T> Observable.Factory.fromIterable(items: Iterable<T>) = object: Observable
     }
 }
 
-fun <T> Observable.Factory.defer(observable: () -> Observable<T>) = object: Observable<T> {
+inline fun <T> Observable.Factory.defer(crossinline observable: () -> Observable<T>) = object: Observable<T> {
     override fun subscribe(subscriber: Subscriber<in T>) {
         val newObservable = observable()
         newObservable.subscribe(subscriber)
